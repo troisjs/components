c2a1fcbba48692620c9a1d7c125f23b867f4bc0b
Kevin Levron <kevin.levron@gmail.com>
2021-10-20 20:29:13 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Slider :images="images" />
+</template>
+
+<script lang="ts">
+import { default as Slider }  from './sliders/Slider1.vue';
+export default {
+  components: { Slider },
+  data() {
+    return {
+      images: [
+        { src: '/assets/images/img1.jpg' },
+        { src: '/assets/images/img2.jpg' },
+        { src: '/assets/images/img3.jpg' },
+        { src: '/assets/images/img4.jpg' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+body, html, #app {
+  margin: 0;
+  height: 100%;
+}
+canvas {
+  display: block;
+}
+</style>
