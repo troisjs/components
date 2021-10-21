f21c43b7d88dac14facdea245449e7e212370fed
Kevin Levron <kevin.levron@gmail.com>
2021-10-21 19:08:33 +0200

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,14 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2020: true,
-    node: true
+    es2020: true
   },
   extends: [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript/recommended',
     'standard'
   ],
   parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script lang="ts">
-import { default as Slider }  from './sliders/displacement/Slider.vue'
-// import { default as Slider }  from './sliders/explode/Slider.vue'
-// import { default as Slider }  from './sliders/points/Slider.vue'
-// import { default as Slider }  from './sliders/zoomblur/Slider.vue'
+import Slider from './sliders/displacement/Slider.vue'
+// import Slider  from './sliders/explode/Slider.vue'
+// import Slider  from './sliders/points/Slider.vue'
+// import Slider  from './sliders/zoomblur/Slider.vue'
 
 const dispMapShader = `
   varying vec2 vUv;
@@ -29,11 +29,11 @@ export default {
         { src: 'https://troisjs.github.io/assets/images/img1.jpg' },
         { src: 'https://troisjs.github.io/assets/images/img2.jpg' },
         { src: 'https://troisjs.github.io/assets/images/img3.jpg' },
-        { src: 'https://troisjs.github.io/assets/images/img4.jpg' },
+        { src: 'https://troisjs.github.io/assets/images/img4.jpg' }
       ],
       dispMapShader
     }
-  },
+  }
 }
 </script>
 
