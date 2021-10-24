43c7f0ef949a03c7c71d0ae4692362c5f1fc0735
Kevin Levron <kevin.levron@gmail.com>
2021-10-24 23:32:49 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,24 @@ import Slider from './sliders/displacement/Slider.vue'
 // import Slider  from './sliders/points/Slider.vue'
 // import Slider  from './sliders/zoomblur/Slider.vue'
 
+import snoise2 from './glsl/snoise2.glsl.js'
+
 const dispMapShader = `
   varying vec2 vUv;
+
+  ${snoise2}
+
   void main() {
-    vec2 uv = vUv * 20.0;
-    float modx = mod(uv.x, 2.0);
-    float mody = mod(uv.y, 2.0);
-    vec2 disp = vec2(0.5, 0.5);
-    if (modx < 1.0) { disp.y = 0.0; } else { disp.y = 1.0; }
-    if (mody < 1.0) { disp.x = 1.0; } else { disp.x = 0.0; }
-    gl_FragColor = vec4(disp.x, disp.y, 0.0, 1.0);
+    vec2 uv = vUv * 50.0;
+    float noise = (snoise(uv) + 1.0)  / 2.0;
+    gl_FragColor = vec4(noise, noise, 0.0, 1.0);
+    // vec2 uv = vUv * 20.0;
+    // float modx = mod(uv.x, 2.0);
+    // float mody = mod(uv.y, 2.0);
+    // vec2 disp = vec2(0.5, 0.5);
+    // if (modx < 1.0) { disp.y = 0.0; } else { disp.y = 1.0; }
+    // if (mody < 1.0) { disp.x = 1.0; } else { disp.x = 0.0; }
+    // gl_FragColor = vec4(disp.x, disp.y, 0.0, 1.0);
   }
 `
 
